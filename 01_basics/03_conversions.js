1198f34cc1c323482c4be3a377aad4c954624cda
AnshulGahlawt <135229106+AnshulGahlawt@users.noreply.github.com>
2025-07-15 07:24:38 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let sc = null
+let vin = Number(sc)
+console.log(vin)
+
+let sc2 = undefined
+let vin2 = Number(sc2)
+console.log(vin2)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false=>0
+
+// 1 => true 0 => false
+// "" => false  "Anshul" => true
+
