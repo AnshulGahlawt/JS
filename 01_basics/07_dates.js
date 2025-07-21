cb0dd3ff08c5b7d26f9bdc5cf3f7f84a08ea8c50
AnshulGahlawt <135229106+AnshulGahlawt@users.noreply.github.com>
2025-07-21 08:14:49 +0000

    date and time done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+
+let created_Date = new Date(2023,0,23);
+// console.log(created_Date.toDateString());
+
+let time_stamp = Date.now();
+console.log(time_stamp);
+console.log(created_Date.getTime());
+
+
