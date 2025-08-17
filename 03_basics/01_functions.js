5d4e716e25310014a5829b5b93d3f472f9376a46
AnshulGahlawt <135229106+AnshulGahlawt@users.noreply.github.com>
2025-08-17 09:29:20 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,10 @@
+function sayMyName(){
+    console.log("A");
+    console.log("n");
+    console.log("s");
+    console.log("h");
+    console.log("u");
+    console.log("l"); 
+}
+// sayMyName()
+
